c116ec7d6a922c32d2fcf2519cac75ec5911eb55
Dominik-Brand <125266410+Dominik-Brand@users.noreply.github.com>
2023-09-19 23:51:01 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define SERVER_IP ""
+#define AUTHENTICATION_TOKEN ""
+
+#define MACHINE_NAME ""
+
+#endif /* CONFIG_H */
